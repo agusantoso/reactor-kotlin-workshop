bbb846971e5433fb186024bddcdb7da6f128942f
agusantoso <agusantoso@gmail.com>
2019-11-17 01:30:24 -0800

    part 11 blocking to reactive

--- src/test/kotlin/io/eddumelendez/reactorkotlin/Part11BlockingToReactive.kt
+++ src/test/kotlin/io/eddumelendez/reactorkotlin/Part11BlockingToReactive.kt
@@ -9,6 +9,8 @@ import org.junit.Assert.assertFalse
 import org.junit.Test
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
+import reactor.core.publisher.toFlux
+import reactor.core.scheduler.Schedulers
 import reactor.test.test
 
 class Part11BlockingToReactive {
@@ -23,9 +25,10 @@ class Part11BlockingToReactive {
                 .verifyComplete()
     }
 
-    // TODO Create a Flux for reading all users from the blocking repository deferred until the flux is subscribed, and run it with an elastic scheduler
+    // Create a Flux for reading all users from the blocking repository deferred until the flux is subscribed, and run it with an elastic scheduler
     fun blockingRepositoryToFlux(repository: BlockingRepository<User>): Flux<User> {
-        return null!!
+        return Flux.defer { repository.findAll().toFlux() }
+                .subscribeOn(Schedulers.elastic())
     }
 
     @Test
@@ -46,7 +49,9 @@ class Part11BlockingToReactive {
 
     // TODO Insert users contained in the Flux parameter in the blocking repository using an parallel scheduler and return a Mono<Void> that signal the end of the operation
     fun fluxToBlockingRepository(flux: Flux<User>, repository: BlockingRepository<User>): Mono<Void> {
-        return null!!
+        return flux.publishOn(Schedulers.elastic())
+                .doOnNext { repository.save(it) }
+                .then()
     }
 
 }
