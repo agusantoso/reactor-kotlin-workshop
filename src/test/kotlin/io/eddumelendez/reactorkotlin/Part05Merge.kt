394d48aa15657eda1db2dd80f7579afb44d3c646
agusantoso <agusantoso@gmail.com>
2019-11-17 01:00:08 -0800

    part 5 merge

--- src/test/kotlin/io/eddumelendez/reactorkotlin/Part05Merge.kt
+++ src/test/kotlin/io/eddumelendez/reactorkotlin/Part05Merge.kt
@@ -24,9 +24,9 @@ class Part05Merge {
                 .verifyComplete()
     }
 
-    // TODO Merge flux1 and flux2 values with interleave
+    // Merge flux1 and flux2 values with interleave
     fun mergeFluxWithInterleave(flux1: Flux<User>, flux2: Flux<User>): Flux<User> {
-        return null!!
+        return flux1.mergeWith(flux2)
     }
 
     @Test
@@ -38,9 +38,9 @@ class Part05Merge {
                 .verifyComplete()
     }
 
-    // TODO Merge flux1 and flux2 values with no interleave (flux1 values and then flux2 values)
+    // Merge flux1 and flux2 values with no interleave (flux1 values and then flux2 values)
     fun mergeFluxWithNoInterleave(flux1: Flux<User>, flux2: Flux<User>): Flux<User> {
-        return null!!
+        return flux1.concatWith(flux2)
     }
 
     @Test
@@ -55,9 +55,9 @@ class Part05Merge {
                 .verifyComplete()
     }
 
-    // TODO Create a Flux containing the value of mono1 then the value of mono2
+    // Create a Flux containing the value of mono1 then the value of mono2
     fun createFluxFromMultipleMono(mono1: Mono<User>, mono2: Mono<User>): Flux<User> {
-        return null!!
+        return Flux.concat(mono1, mono2)
     }
 
 }
